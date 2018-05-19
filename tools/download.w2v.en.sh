0d173ee033f1f7cd5e0415e6d836e99204a8c657
Michał Sitko <mic.sitko@gmail.com>
2018-05-20 00:24:09 +0200

    exec permition for scripts

--- tools/download.w2v.en.sh
+++ tools/download.w2v.en.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 [ -d data ] || mkdir data
 
 wget "https://s3-us-west-1.amazonaws.com/fasttext-vectors/wiki.en.zip" -O $PWD"/data/wiki.en.zip"
--- tools/download.w2v.en.simple.sh
+++ tools/download.w2v.en.simple.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 [ -d data ] || mkdir data
 
 wget "https://www.dropbox.com/s/sg4yxvzg5rysruy/wiki.simple.vec.zip?dl=0" -O $PWD"/data/wiki.simple.vec.zip"
